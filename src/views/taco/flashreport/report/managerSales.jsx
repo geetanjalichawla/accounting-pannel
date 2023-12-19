9ff495c9cdc97126102e2d9e00ccf8a700ce57eb
Meshvi Shah <90126181+MeshviShah@users.noreply.github.com>
2023-12-19 15:38:38 +0530

    Update managerSales.jsx

--- src/views/taco/flashreport/report/managerSales.jsx
+++ src/views/taco/flashreport/report/managerSales.jsx
@@ -104,36 +104,36 @@ useEffect(() => {
           {filteredData.map((row) => (
             <Tr key={row._id}>
               <Td >{row.storeName}</Td>
-              <Td >{row.dailySalesCY}</Td>
-              <Td >{row.dailySalesLY}</Td>
-              <Td >{row.dailySalesCY - row.dailySalesLY}</Td>
+              <Td >{row.dailySalesCY.toFixed(2)}</Td>
+              <Td >{row.dailySalesLY.toFixed(2)}</Td>
+              <Td >{(row.dailySalesCY - row.dailySalesLY).toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.dailySalesCY - row.dailySalesLY, row.dailySalesCY).toFixed(2)}%
               </Td>
-              <Td >{row.WTDSalesCY}</Td>
-              <Td >{row.WTDSalesLY}</Td>
-              <Td >{row.WTDSalesCY - row.WTDSalesLY}</Td>
+              <Td >{row.WTDSalesCY.toFixed(2)}</Td>
+              <Td >{row.WTDSalesLY.toFixed(2)}</Td>
+              <Td >{(row.WTDSalesCY - row.WTDSalesLY).toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.WTDSalesCY - row.WTDSalesLY, row.WTDSalesCY).toFixed(2)}%
               </Td>
               {/* PTD Sales Data */}
-              <Td >{row.PTDSalesCY}</Td>
-              <Td >{row.PTDSalesLY}</Td>
-              <Td >{row.PTDSalesCY - row.PTDSalesLY}</Td>
+              <Td >{row.PTDSalesCY.toFixed(2)}</Td>
+              <Td >{row.PTDSalesLY.toFixed(2)}</Td>
+              <Td >{(row.PTDSalesCY - row.PTDSalesLY).toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.PTDSalesCY - row.PTDSalesLY, row.PTDSalesCY).toFixed(2)}%
               </Td>
               {/* YTD Sales Data */}
-              <Td >{row.YTDSalesCY}</Td>
-              <Td >{row.YTDSalesLY}</Td>
-              <Td >{row.YTDSalesCY - row.YTDSalesLY}</Td>
+              <Td >{row.YTDSalesCY.toFixed(2)}</Td>
+              <Td >{row.YTDSalesLY.toFixed(2)}</Td>
+              <Td >{(row.YTDSalesCY - row.YTDSalesLY).toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.YTDSalesCY - row.YTDSalesLY, row.YTDSalesCY).toFixed(2)}%
               </Td>
               {/* R13 Sales Data */}
-              <Td >{row.R13SalesCY}</Td>
-              <Td >{row.R13SalesLY}</Td>
-              <Td >{row.R13SalesCY - row.R13SalesLY}</Td>
+              <Td >{row.R13SalesCY.toFixed(2)}</Td>
+              <Td >{row.R13SalesLY.toFixed(2)}</Td>
+              <Td >{(row.R13SalesCY - row.R13SalesLY).toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.R13SalesCY - row.R13SalesLY, row.R13SalesCY).toFixed(2)}%
               </Td>
@@ -141,38 +141,38 @@ useEffect(() => {
           ))}
            <Tr>
            <Td >Total</Td>
-           <Td >{calculateTotal('dailySalesCY',filteredData)}</Td>
-            <Td >{calculateTotal('dailySalesLY',filteredData)}</Td>
-            <Td >{calculateTotal('dailySalesCY',filteredData) - calculateTotal('dailySalesLY',filteredData)}</Td>
+           <Td >{calculateTotal('dailySalesCY',filteredData).toFixed(2)}</Td>
+            <Td >{calculateTotal('dailySalesLY',filteredData).toFixed(2)}</Td>
+            <Td >{(calculateTotal('dailySalesCY',filteredData) - calculateTotal('dailySalesLY',filteredData)).toFixed(2)}</Td>
             <Td >
               {calculatePercentage(calculateTotal('dailySalesCY',filteredData) - calculateTotal('dailySalesLY',filteredData), calculateTotal('dailySalesCY',filteredData)).toFixed(2)}%
             </Td>
-            <Td >{calculateTotal('WTDSalesCY',filteredData)}</Td>
-  <Td >{calculateTotal('WTDSalesLY',filteredData)}</Td>
-  <Td >{calculateTotal('WTDSalesCY',filteredData) - calculateTotal('WTDSalesLY',filteredData)}</Td>
+            <Td >{calculateTotal('WTDSalesCY',filteredData).toFixed(2)}</Td>
+  <Td >{calculateTotal('WTDSalesLY',filteredData).toFixed(2)}</Td>
+  <Td >{(calculateTotal('WTDSalesCY',filteredData) - calculateTotal('WTDSalesLY',filteredData)).toFixed(2)}</Td>
   <Td >
     {calculatePercentage(calculateTotal('WTDSalesCY',filteredData) - calculateTotal('WTDSalesLY',filteredData), calculateTotal('WTDSalesCY',filteredData)).toFixed(2)}%
   </Td>
             {/* Add other columns as needed */}
-            <Td >{calculateTotal('PTDSalesCY',filteredData)}</Td>
-  <Td >{calculateTotal('PTDSalesLY',filteredData)}</Td>
-  <Td >{calculateTotal('PTDSalesCY',filteredData) - calculateTotal('PTDSalesLY',filteredData)}</Td>
+            <Td >{calculateTotal('PTDSalesCY',filteredData).toFixed(2)}</Td>
+  <Td >{calculateTotal('PTDSalesLY',filteredData).toFixed(2)}</Td>
+  <Td >{(calculateTotal('PTDSalesCY',filteredData) - calculateTotal('PTDSalesLY',filteredData)).toFixed(2)}</Td>
   <Td >
     {calculatePercentage(calculateTotal('PTDSalesCY',filteredData) - calculateTotal('PTDSalesLY',filteredData), calculateTotal('PTDSalesCY',filteredData)).toFixed(2)}%
   </Td>
 
   {/* YDT Sales */}
-  <Td >{calculateTotal('YTDSalesCY',filteredData)}</Td>
-  <Td >{calculateTotal('YTDSalesLY',filteredData)}</Td>
-  <Td >{calculateTotal('YTDSalesCY',filteredData) - calculateTotal('YTDSalesLY',filteredData)}</Td>
+  <Td >{calculateTotal('YTDSalesCY',filteredData).toFixed(2)}</Td>
+  <Td >{calculateTotal('YTDSalesLY',filteredData).toFixed(2)}</Td>
+  <Td >{(calculateTotal('YTDSalesCY',filteredData) - calculateTotal('YTDSalesLY',filteredData)).toFixed(2)}</Td>
   <Td >
     {calculatePercentage(calculateTotal('YTDSalesCY',filteredData) - calculateTotal('YTDSalesLY',filteredData), calculateTotal('YTDSalesCY',filteredData)).toFixed(2)}%
   </Td>
 
   {/* R13 Sales */}
-  <Td >{calculateTotal('R13SalesCY',filteredData)}</Td>
-  <Td >{calculateTotal('R13SalesLY',filteredData)}</Td>
-  <Td >{calculateTotal('R13SalesCY',filteredData) - calculateTotal('R13SalesLY',filteredData)}</Td>
+  <Td >{calculateTotal('R13SalesCY',filteredData).toFixed(2)}</Td>
+  <Td >{calculateTotal('R13SalesLY',filteredData).toFixed(2)}</Td>
+  <Td >{(calculateTotal('R13SalesCY',filteredData) - calculateTotal('R13SalesLY',filteredData)).toFixed(2)}</Td>
   <Td >
     {calculatePercentage(calculateTotal('R13SalesCY',filteredData) - calculateTotal('R13SalesLY',filteredData), calculateTotal('R13SalesCY',filteredData)).toFixed(2)}%
   </Td>
@@ -239,12 +239,12 @@ useEffect(() => {
             <Tr key={row._id}>
               <Td >{row.storeName}</Td>
               {/* Net Sales Data */}
-              <Td >{row.dailySalesCY}</Td>
+              <Td >{row.dailySalesCY.toFixed(2)}</Td>
              
               {/* Transaction Count Data */}
-              <Td >{row.dailyTransCountCY}</Td>
-              <Td >{row.dailyTransCountLY}</Td>
-              <Td >{row.dailyTransCountCY - row.dailyTransCountLY}</Td>
+              <Td >{row.dailyTransCountCY.toFixed(2)}</Td>
+              <Td >{row.dailyTransCountLY.toFixed(2)}</Td>
+              <Td >{(row.dailyTransCountCY - row.dailyTransCountLY).toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.dailyTransCountCY - row.dailyTransCountLY, row.dailyTransCountCY).toFixed(2)}%
               </Td>
@@ -265,35 +265,35 @@ useEffect(() => {
              
               {/* Cash Data */}
               {/* ... add Cash data columns similarly */}
-              <Td >{row.CashOverShort}</Td>
+              <Td >{(row.CashOverShort).toFixed(2)}</Td>
               {/* Del After Data */}
               {/* ... add Del After data columns similarly */}
              
-              <Td >{row.DeletionsAfter}</Td>
+              <Td >{row.DeletionsAfter.toFixed(2)}</Td>
               <Td >
                 {calculatePercentage(row.DeletionsAfter , row.dailySalesCY).toFixed(2)}%
               </Td>
               {/* ICOS Data */}
-              <Td >{row.ICOSVarPercent}</Td>
-              <Td >{row.WTDICOSVarPercent}</Td>
+              <Td >{row.ICOSVarPercent.toFixed(2)}</Td>
+              <Td >{row.WTDICOSVarPercent.toFixed(2)}</Td>
             </Tr>
           ))}
           <Tr>
     <Td >Total</Td>
-    <Td >{calculateTotal('dailySalesCY',filteredData)}</Td>
-    <Td >{calculateTotal('dailyTransCountCY',filteredData)}</Td>
-    <Td >{calculateTotal('dailyTransCountLY',filteredData)}</Td>
-    <Td >{calculateTotal('dailyTransCountCY',filteredData) - calculateTotal('dailyTransCountLY',filteredData)}</Td>
+    <Td >{calculateTotal('dailySalesCY',filteredData).toFixed(2)}</Td>
+    <Td >{calculateTotal('dailyTransCountCY',filteredData).toFixed(2)}</Td>
+    <Td >{calculateTotal('dailyTransCountLY',filteredData).toFixed(2)}</Td>
+    <Td >{(calculateTotal('dailyTransCountCY',filteredData) - calculateTotal('dailyTransCountLY',filteredData)).toFixed(2)}</Td>
     <Td >
       {calculatePercentage(
         calculateTotal('dailyTransCountCY',filteredData) - calculateTotal('dailyTransCountLY',filteredData),
         calculateTotal('dailyTransCountCY',filteredData)
       ).toFixed(2)}%
     </Td>
-    <Td >{calculateTotal('dailySalesCY',filteredData)}</Td>
-    <Td >{calculateTotal('dailySalesLY',filteredData)}</Td>
+    <Td >{calculateTotal('dailySalesCY',filteredData).toFixed(2)}</Td>
+    <Td >{calculateTotal('dailySalesLY',filteredData).toFixed(2)}</Td>
     <Td >
-      {calculateTotal('dailySalesCY',filteredData) - calculateTotal('dailySalesLY',filteredData)}
+      {(calculateTotal('dailySalesCY',filteredData) - calculateTotal('dailySalesLY',filteredData)).toFixed(2)}
     </Td>
     <Td >
       {calculatePercentage(
@@ -301,17 +301,17 @@ useEffect(() => {
         calculateTotal('dailySalesCY',filteredData)
       ).toFixed(2)}%
     </Td>
-    <Td >{calculateTotal('CashOverShort',filteredData)}</Td>
+    <Td >{calculateTotal('CashOverShort',filteredData).toFixed(2)}</Td>
     {/* Add total for Del After */}
-    <Td >{calculateTotal('DeletionsAfter',filteredData)}</Td>
+    <Td >{calculateTotal('DeletionsAfter',filteredData).toFixed(2)}</Td>
     <Td >
-      {calculatePercentage(calculateTotal('DeletionsAfter',filteredData), calculateTotal('dailySalesCY',filteredData)).toFixed(2)}%
+      {(calculatePercentage(calculateTotal('DeletionsAfter',filteredData), calculateTotal('dailySalesCY',filteredData))).toFixed(2)}%
     </Td>
     
     {/* Add total for Del After */}
-    <Td >{calculateTotal('ICOSVarPercent',filteredData)}</Td>
+    <Td >{calculateTotal('ICOSVarPercent',filteredData).toFixed(2)}</Td>
     <Td >
-      {calculateTotal('WTDICOSVarPercent',filteredData)}
+      {calculateTotal('WTDICOSVarPercent',filteredData).toFixed(2)}
     </Td>
     {/* ... add total for other columns similarly */}
   </Tr>
